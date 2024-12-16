68efab5f90aec6a10e2d9990e61e0548dbf68278
Michael Mapanao <michael.mapanao@gmail.com>
2024-12-16 12:21:29 -0800

    Added Husky Sprite
    Sprite added, animations added

--- Characters/dog_player.gd
+++ Characters/dog_player.gd
@@ -1,36 +1,36 @@
 extends CharacterBody2D
-@export var move_speed : float = 50
+
+@export var move_speed: float = 50
 @onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
-	# get input direction
+	# Get input direction
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)	
-
-
-	var direction = Input.get_axis("left", "right")
-	if direction > 0:
+	)
+	
+	# Default to idle animation
+	var animation = "idle"
+	
+	# Determine animation based on input
+	if input_direction.x > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0:
+		animation = "walk_right"
+	elif input_direction.x < 0:
 		animated_sprite.flip_h = true
+		animation = "walk_right"
+	elif input_direction.y < 0:  # Upward movement
+		animation = "walk_up"
+	elif input_direction.y > 0:  # Downward movement
+		animation = "walk_down"
 	
-	#Play animations
-	if direction == 0:
-		animated_sprite.play("idle")
-	else:
-		animated_sprite.play("walk") 
+	# Play the determined animation if it changes
+	if animated_sprite.animation != animation:
+		animated_sprite.play(animation)
 	
-	if Input.is_action_pressed("bite"):
-		animated_sprite.play("bite")
-	
-	if direction:
-			velocity.x = direction * move_speed
-	else:
-			velocity.x = move_toward(velocity.x, 0 , move_speed)
-	
-	print(input_direction)
-	#update velocity
+	# Update velocity
 	velocity = input_direction * move_speed
-	#Move and slide function uses velocity of character body to move character on map
+	
+	# Move the character
 	move_and_slide()
